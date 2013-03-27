09f36cfe5a6fc5f7c72be59e02bfc620a2d95566
genya0407 <longinus.eva+git@gmail.com>
2013-03-27 14:14:08 +0900

    fifth

--- p17.d
+++ p17.d
@@ -1,42 +1,52 @@
 import std.stdio;
 
+string str_conv(string base_str, string new_str){
+	base_str ~= new_str;
+	//writeln(base_str);
+	return base_str;
+}
+
 void main(){
-	string _1_19 = "one" ~ "two" ~ "three" ~ "four" ~ "five" ~ "six" ~ "seven" ~ "eight" ~ "nine" ~ "ten" ~ "eleven" ~ "twenty" ~ "thirteen" ~ "fourteen" ~ "fifteen" ~ "sixteen" ~ "seventeen" ~ "eighteen" ~ "nineteen";
-	string _1 = "one";
-	string _2 = "two";
-	string _3 = "three";
-	string _4 = "four";
-	string _5 = "five";
-	string _6 = "six";
-	string _7 = "seven";
-	string _8 = "eight";
-	string _9 = "nine";
-	string _1_9 = "one" ~ "two" ~ "three" ~ "four" ~ "five" ~ "six" ~ "seven" ~ "eight" ~ "nine";
-	string _10 = "ten";
-	string _11 = "eleven";
-	string _12 = "twenty";
-	string _13 = "thirteen";
-	string _14 = "fourteen";
-	string _15 = "fifteen";
-	string _16 = "sixteen";
-	string _17 = "seventeen";
-	string _18 = "eighteen";
-	string _19 = "nineteen";
-	string _20_90 = ["twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety"];
-	string _20 = "twenty";
-	string _30 = "thirty";
-	string _40 = "fourty";
-	string _50 = "fifty";
-	string _60 = "sixty";
-	string _70 = "seventy";
-	string _80 = "eighty";
-	string _90 = "ninety";
-	string _100 = "hundred";
-	writeln(_1_19.length);
+	string[] one_ten         = ["","one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
+	string[] one_nine        = ["","one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+	string[] one_nine_nzero  = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+	string[] eleven_nineteen = ["eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+	string[] twenty_ninety   = ["twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"];
+	string   hundred         = "hundred";
+	string   and             = "and";
+	string   thousand        = "thousand";
 	
-	int a;
-	foreach(elem; _20_90){ 
-		a += elem.length*10 + _1_9.length;
+	string   base_str = "";
+	
+	foreach(elem; one_ten){
+		base_str = str_conv(base_str,elem);
 	}
+	foreach(elem; eleven_nineteen){
+		base_str = str_conv(base_str,elem);
+	}
+	foreach(ten_d; twenty_ninety){
+		foreach(one_d; one_nine){
+			base_str = str_conv(base_str,ten_d ~ one_d);
+		}
+	}
+	foreach(hundred_d; one_nine_nzero){
+		foreach(elem; one_ten){
+			if(elem == ""){
+				base_str = str_conv(base_str,hundred_d ~ hundred ~ elem); // [one] hundred and 
+			}else{
+				base_str = str_conv(base_str,hundred_d ~ hundred ~ and ~ elem);
+			}
+		}
+		foreach(elem; eleven_nineteen){
+			base_str = str_conv(base_str,hundred_d ~ hundred ~ and ~ elem);
+		}
+		foreach(ten_d; twenty_ninety){
+			foreach(one_d; one_nine){
+				base_str = str_conv(base_str,hundred_d ~ hundred ~ and ~ ten_d ~ "" ~ one_d);
+			}
+		}
+	}
+	base_str = str_conv(base_str,"one" ~ thousand);
+	writeln(base_str.length);
 	
 }
